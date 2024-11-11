c8add1b9f44b7d28d9c6d15737e0a422705b5c1a
Dnevend <Dnevend@gmail.com>
2024-11-11 19:12:37 +0800

    feat: matomo

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueMatomo from 'vue-matomo'
 
 const app = createApp(App)
+
+app.use(VueMatomo, {
+    host: 'https://dnevendsite.matomo.cloud/',
+    siteId: 2
+});
 
 app.use(router)
 
 app.mount('#app')
+
+// @ts-ignore
+window._paq.push(['trackPageView']); // 跟踪页面视图
